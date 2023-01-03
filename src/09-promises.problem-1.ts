72d74b9a09ca2800387d68dafb8537f6538c59dc
estebanrojasl <esteban7590@hotmail.com>
2023-01-03 14:39:42 -0500

    process

--- src/09-promises.problem-1.ts
+++ src/09-promises.problem-1.ts
@@ -9,10 +9,10 @@ interface LukeSkywalker {
   gender: string;
 }
 
-export const fetchLukeSkywalker = async (): Promise<LukeSkywalker> => {
-  const data = await fetch('https://swapi.dev/api/people/1').then((res) => {
+export const fetchLukeSkywalker = async () => {
+  const data = (await fetch('https://swapi.dev/api/people/1').then((res) => {
     return res.json();
-  });
+  })) as LukeSkywalker;
 
   return data;
 };
